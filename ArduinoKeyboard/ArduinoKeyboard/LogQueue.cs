ffdb4ae20ab2d6f48fb2bbd9bc81133cdeb4806e
eugenioaugusto <37295360+eugenioaugusto@users.noreply.github.com>
2018-03-28 10:33:43 -0300

    Terminando classe de log

--- ArduinoKeyboard/ArduinoKeyboard/LogQueue.cs
+++ ArduinoKeyboard/ArduinoKeyboard/LogQueue.cs
@@ -1,12 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
 using System.Threading;
 
-namespace ArduinoConnect
-
+namespace ArduinoKeyboard
 {
 	/// <summary>
 	/// classe de LOG
@@ -16,21 +15,28 @@ namespace ArduinoConnect
 		private bool bStop = false;
 		//TODO arrumar a classe de log. criar uma mais simples
 		private static ConcurrentQueue<TagLogData> queueLogFile = new ConcurrentQueue<TagLogData>();
+		private String filename;
+
+		public static ConcurrentQueue<TagLogData> QueueLogFile { get => queueLogFile; set => queueLogFile = value; }
+
+		public LogQueue(String filename)
+		{
+			this.filename = filename;
+		}
 		/// <summary>
 		/// método usado na thread de log
 		/// </summary>
 		public void LogFile()
 		{
-			string fileLogName = "";
-			StreamWriter file_log = new StreamWriter(new FileStream(fileLogName, System.IO.FileMode.Append));
+			StreamWriter file_log = new StreamWriter(new FileStream(this.filename, System.IO.FileMode.Append));
 			string text_buffer;
 			TimeSpan delay_hig = new TimeSpan(0, 0, 0, 2, 0);
 			TagLogData stLogData;
 			while (!this.bStop)
 			{
-				while (queueLogFile.Count > 0)
+				while (QueueLogFile.Count > 0)
 				{
-					if (!queueLogFile.TryDequeue(out stLogData))
+					if (!QueueLogFile.TryDequeue(out stLogData))
 					{
 						Thread.Sleep(100);
 						continue;
@@ -44,18 +50,15 @@ namespace ArduinoConnect
 					file_log.Write(text_buffer);
 					file_log.Flush();
 				}
-				//TODO criar evento na classe pai statico para quando deve sair
-				/**
-				if (ServidorComponentes.G_ShutdownEvent.WaitOne(delay_hig, true))
+				if (ArduinoKeyboardService.G_ShutdownEvent.WaitOne(delay_hig, true))
 				{
 					this.bStop = true;
-				}*/
+				}
 			}
 		}
 		public void Stop()
 		{
 			this.bStop = true;
-
 		}
 	}
 }
