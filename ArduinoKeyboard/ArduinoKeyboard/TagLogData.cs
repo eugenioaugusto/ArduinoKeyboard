6b35450ef1253b969a0f21791bc70142934c0e14
eugenioaugusto <37295360+eugenioaugusto@users.noreply.github.com>
2018-03-28 10:32:45 -0300

    Adicionando iClonable

--- ArduinoKeyboard/ArduinoKeyboard/TagLogData.cs
+++ ArduinoKeyboard/ArduinoKeyboard/TagLogData.cs
@@ -1,16 +1,20 @@
 using System;
-namespace ArduinoConnect
-
+namespace ArduinoKeyboard
 {
     /// <summary>
     /// classe de LOG
     /// </summary>
-    public class TagLogData
+    public class TagLogData : ICloneable
     {
         public DateTime DtCurrTime {get; set;}
 
         public Int32 Text_linha { get; internal set; }
         public string Text_function { get; internal set; }
         public string Text_data { get; internal set; }
-    }
+
+		public object Clone()
+		{
+			return this.MemberwiseClone();
+		}
+	}
 }
