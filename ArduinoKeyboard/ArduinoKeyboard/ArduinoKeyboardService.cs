a99578d1ea3d1b00054fbe5ebb11c52484d20ee9
eugenioaugusto <37295360+eugenioaugusto@users.noreply.github.com>
2018-04-16 16:04:13 -0300

    Aumentando instrumentação

--- ArduinoKeyboard/ArduinoKeyboard/ArduinoKeyboardService.cs
+++ ArduinoKeyboard/ArduinoKeyboard/ArduinoKeyboardService.cs
@@ -16,171 +16,202 @@ using vJoyInterfaceWrap;
 
 namespace ArduinoKeyboard
 {
-    public enum TipoBotao
-    {
-        btn_1 = 1,
-        btn_2 = 2,
-        btn_3 = 3,
-        btn_4 = 4,
-        btn_5 = 5,
-        btn_6 = 6,
-        btn_7 = 7,
-        btn_8 = 8,
-        btn_9 = 9,
-        btn_10 = 10
-    }
-    public partial class ArduinoKeyboardService : ServiceBase
-    {
-        private const string LOG_FILE_NAME = @"C:\arduino keyboard\log\log.txt";
-        private const string LOG_CRITICAL_FILE_NAME = @"C:\arduino keyboard\log\logCritical.txt";
-        private const string CNTL_PATH = @"C:\arduino keyboard\cntl\";
-        private LogQueue logQueue;
-        private bool stop = false;
-        private static ManualResetEvent g_ShutdownEvent;
-        private static string g_strIniFileName = "config.ini";
-        private static Dictionary<String, ArduinoConnect> mapConnects = new Dictionary<String, ArduinoConnect>();
-        private static Configs config;
-        private static vJoy joystick;
-        private static vJoy.JoystickState iReport;
-        FileSystemWatcher watch;
+	public enum TipoBotao
+	{
+		btn_1 = 1,
+		btn_2 = 2,
+		btn_3 = 3,
+		btn_4 = 4,
+		btn_5 = 5,
+		btn_6 = 6,
+		btn_7 = 7,
+		btn_8 = 8,
+		btn_9 = 9,
+		btn_10 = 10
+	}
+	public partial class ArduinoKeyboardService : ServiceBase
+	{
+		private const string LOG_FILE_NAME = @"C:\arduino keyboard\log\log.txt";
+		private const string LOG_CRITICAL_FILE_NAME = @"C:\arduino keyboard\log\logCritical.txt";
+		private const string CNTL_PATH = @"C:\arduino keyboard\cntl\";
+		private LogQueue logQueue;
+		private bool stop = false;
+		private static ManualResetEvent g_ShutdownEvent;
+		private static string g_strIniFileName = "config.ini";
+		private static Dictionary<String, ArduinoConnect> mapConnects = new Dictionary<String, ArduinoConnect>();
+		private static Configs config;
+		private static vJoy joystick;
+		private static vJoy.JoystickState iReport;
+		FileSystemWatcher watch;
 
-        public static ManualResetEvent G_ShutdownEvent { get => g_ShutdownEvent; set => g_ShutdownEvent = value; }
+		public static ManualResetEvent G_ShutdownEvent { get => g_ShutdownEvent; set => g_ShutdownEvent = value; }
 
-        public ArduinoKeyboardService()
-        {
-            InitializeComponent();
-        }
+		public ArduinoKeyboardService()
+		{
+			InitializeComponent();
+		}
 
-        protected override void OnStart(string[] args)
-        {
-            this.ServiceInit();
-        }
+		protected override void OnStart(string[] args)
+		{
+			this.ServiceInit();
+		}
 
-        protected override void OnStop()
-        {
-            this.logQueue.Stop();
-            g_ShutdownEvent.Set();
-            Thread.Sleep(1000);
-        }
-        public static void LogInfo(String comPort, String log)
-        {
-            if (config.LogInfo)
-            {
-                Log(comPort, log);
-            }
-        }
-        public void LogDataReceived(String comPort, String log)
-        {
-            if (config.LogDataReceived)
-            {
-                Log(comPort, log);
-            }
-        }
-        public static void Log(String comInfo, String text)
-        {
-            TagLogData data = new TagLogData();
-            data.Text_data = String.Format("ComPort {0}: {1}", comInfo, text);
-            data.DtCurrTime = DateTime.Now;
-            LogQueue.QueueLogFile.Enqueue(data);
-        }
-        /// <summary>
-        /// Método que inicializa as threads de leitura
-        /// </summary>
-        public void ServiceInit()
-        {
-            G_ShutdownEvent = new ManualResetEvent(false);
-            //inicializa Thread de log
-            this.logQueue = new LogQueue(LOG_FILE_NAME);
-            Thread t = new Thread(this.logQueue.LogFile);
-            t.Start();
-
-
-            string msg = "Comando Start recebido.";
-            Log("Main", msg);
-            watch = new FileSystemWatcher(CNTL_PATH, "*.ini");
-            watch.NotifyFilter = NotifyFilters.LastWrite;
-            FileSystemEventHandler evento = new FileSystemEventHandler(OnIniChanged);
-
-            watch.Changed += evento;
-            watch.Created += evento;
-            watch.Deleted += evento;
-            watch.Renamed += new RenamedEventHandler(OnIniChanged);
+		protected override void OnStop()
+		{
+			this.logQueue.Stop();
+			g_ShutdownEvent.Set();
+			Thread.Sleep(1000);
+		}
+		private static void LogInfo(String log)
+		{
+			LogInfo("Main", log);
+		}
+		private static void LogSevere(String log)
+		{
+			Log("Main", log);
+		}
+		public static void LogInfo(String comPort, String log)
+		{
+			if (config.LogInfo)
+			{
+				Log(comPort, log);
+			}
+		}
+		public void LogDataReceived(String comPort, String log)
+		{
+			if (config.LogDataReceived)
+			{
+				Log(comPort, log);
+			}
+		}
+		public static void Log(String comInfo, String log)
+		{
+			TagLogData data = new TagLogData();
+			data.Text_data = String.Format("ComPort {0}: {1}", comInfo, log);
+			data.DtCurrTime = DateTime.Now;
+			LogQueue.QueueLogFile.Enqueue(data);
+		}
+		/// <summary>
+		/// Método que inicializa as threads de leitura
+		/// </summary>
+		public void ServiceInit()
+		{
+			G_ShutdownEvent = new ManualResetEvent(false);
+			//inicializa Thread de log
+			this.logQueue = new LogQueue(LOG_FILE_NAME);
+			Thread t = new Thread(this.logQueue.LogFile);
+			t.Start();
 
 
-            watch.EnableRaisingEvents = true;
-            try
-            {
-                if (!LeConfiguracao())
-                {
-                    msg = "Falha não tratada ao ler arquivo de configuração.";
-                    StreamWriter file_log = new StreamWriter(new FileStream(LOG_CRITICAL_FILE_NAME, System.IO.FileMode.Append));
-                    file_log.WriteLine(msg);
-                    file_log.Close();
-                    base.Stop();
-                    return;
-                }
-            }
-            catch (Exception ex)
-            {
-                msg = "Exceção capturada ao ler arquivo de configuração : " + ex.Message;
-                if (!File.Exists(LOG_CRITICAL_FILE_NAME))
-                {
-                    File.Create(LOG_CRITICAL_FILE_NAME).Close();
-                }
-                StreamWriter file_log = new StreamWriter(new FileStream(LOG_CRITICAL_FILE_NAME, System.IO.FileMode.Append));
-                file_log.WriteLine("[Exception][le_configuracao()]Exceção ocorrida durante tentativa leitura de arquivo de configuração, causa : " + ex.ToString() + " - " + ex.StackTrace);
-                file_log.Close();
-                base.Stop();
-                return;
-            }
-            //iniciliza método de leitura
-            t = new Thread(this.Run);
-            t.Start();
-        }
-        private static void OnIniChanged(object source, FileSystemEventArgs e)
-        {
-            string filename = Path.GetFileName(e.FullPath).ToLower();
-            if (filename == g_strIniFileName)
-            {
-                FileSystemWatcher fileSorce = (FileSystemWatcher)source;
-                fileSorce.EnableRaisingEvents = false;
-                TagLogData stLogData = new TagLogData();
-                stLogData.DtCurrTime = DateTime.Now;
-                try
-                {
-                    LeConfiguracao();
-                    stLogData.Text_data = "Detectou alteração no arquivo de configuração.";
-                    LogQueue.QueueLogFile.Enqueue((TagLogData)stLogData.Clone());
-                }
-                catch (Exception ex)
-                {
-                    stLogData.Text_data = "Exceção ao ler arquivo de configuração : " + ex.Message + " - " + ex.StackTrace;
-                    LogQueue.QueueLogFile.Enqueue((TagLogData)stLogData.Clone());
-                }
-                fileSorce.EnableRaisingEvents = true;
-            }
-        }
-        public static bool pressButton(TipoBotao tipoBotao, String comPort)
-        {
-            return joystick.SetBtn(true, 1, (uint)tipoBotao);
-        }
-        public static bool releaseButton(TipoBotao tipoBotao, String comPort)
-        {
-            return joystick.SetBtn(false, 1, (uint)tipoBotao);
-        }
-        private static bool LeConfiguracao()
-        {
-            if (config == null)
-            {
-                config = new Configs();
-            }
-            config.LogInfo = true;
-            config.SleepNotExist = 30;
-            config.SleepTime = 1;
-            config.ListRepeticoes = new int[] { 10, 25, 2 };
-            config.IsRepeat = new bool[] { false, false, false, false, false, false, false, false, false, false, false };
-            return true;
-            /*
+			string msg = "Comando Start recebido.";
+			LogSevere(msg);
+			watch = new FileSystemWatcher(CNTL_PATH, "*.ini");
+			watch.NotifyFilter = NotifyFilters.LastWrite;
+			FileSystemEventHandler evento = new FileSystemEventHandler(OnIniChanged);
+
+			watch.Changed += evento;
+			watch.Created += evento;
+			watch.Deleted += evento;
+			watch.Renamed += new RenamedEventHandler(OnIniChanged);
+
+
+			watch.EnableRaisingEvents = true;
+			try
+			{
+				if (!LeConfiguracao())
+				{
+					msg = "Falha não tratada ao ler arquivo de configuração.";
+					StreamWriter file_log = new StreamWriter(new FileStream(LOG_CRITICAL_FILE_NAME, System.IO.FileMode.Append));
+					file_log.WriteLine(msg);
+					file_log.Close();
+					base.Stop();
+					return;
+				}
+				LogInfo("Leu Arquivo de Configuração");
+			}
+			catch (Exception ex)
+			{
+				msg = String.Format("Exceção capturada ao ler arquivo de configuração : {0}\nStack:{1}", ex.Message, ex.StackTrace);
+				if (!File.Exists(LOG_CRITICAL_FILE_NAME))
+				{
+					File.Create(LOG_CRITICAL_FILE_NAME).Close();
+				}
+				StreamWriter file_log = new StreamWriter(new FileStream(LOG_CRITICAL_FILE_NAME, System.IO.FileMode.Append));
+				file_log.WriteLine("[Exception][le_configuracao()]Exceção ocorrida durante tentativa leitura de arquivo de configuração, causa : " + ex.ToString() + " - " + ex.StackTrace);
+				file_log.Close();
+				base.Stop();
+				return;
+			}
+			LogSevere("Preparando joystick");
+			joystick = new vJoy();
+			iReport = new vJoy.JoystickState();
+			if (!joystick.vJoyEnabled())
+			{
+				LogSevere("Joystick não está funcionando");
+				base.Stop();
+				return;
+			}
+			VjdStat status = joystick.GetVJDStatus(JOYSTICK_ID);
+			// Acquire the target
+			if ((status == VjdStat.VJD_STAT_OWN) || ((status == VjdStat.VJD_STAT_FREE) && (!joystick.AcquireVJD(JOYSTICK_ID))))
+			{
+				LogSevere("Falha ao carregar status do joystick 1");
+				LogSevere(String.Format("Status = [{0}]", status));
+				base.Stop();
+				return;
+			}
+			else
+			{
+				LogSevere("Carregou o joystick " + JOYSTICK_ID);
+			}
+			joystick.ResetVJD(JOYSTICK_ID);
+			//iniciliza método de leitura
+			t = new Thread(this.Run);
+			t.Start();
+			LogInfo("Thread Principal iniciada");
+		}
+		private static void OnIniChanged(object source, FileSystemEventArgs e)
+		{
+			string filename = Path.GetFileName(e.FullPath).ToLower();
+			if (filename == g_strIniFileName)
+			{
+				FileSystemWatcher fileSorce = (FileSystemWatcher)source;
+				fileSorce.EnableRaisingEvents = false;
+				try
+				{
+					TagLogData stLogData = new TagLogData();
+					stLogData.DtCurrTime = DateTime.Now;
+					LogSevere("Detectou alteração no arquivo de configuração.");
+					LeConfiguracao();
+				}
+				catch (Exception ex)
+				{
+					LogSevere(String.Format("Exceção ao ler arquivo de configuração : {0}\nStack:{1}", ex.Message, ex.StackTrace));
+				}
+				fileSorce.EnableRaisingEvents = true;
+			}
+		}
+		public static bool pressButton(TipoBotao tipoBotao, String comPort)
+		{
+			return joystick.SetBtn(true, JOYSTICK_ID, (uint)tipoBotao);
+		}
+		public static bool releaseButton(TipoBotao tipoBotao, String comPort)
+		{
+			return joystick.SetBtn(false, JOYSTICK_ID, (uint)tipoBotao);
+		}
+		private static bool LeConfiguracao()
+		{
+			if (config == null)
+			{
+				config = new Configs();
+			}
+			config.LogInfo = true;
+			config.SleepNotExist = 30;
+			config.SleepTime = JOYSTICK_ID;
+			config.ListRepeticoes = new int[] { 10, 25, 2 };
+			config.IsRepeat = new bool[] { false, false, false, false, false, false, false, false, false, false, false };
+			return true;
+			/*
             if ( !File.Exists(CNTL_PATH + g_strIniFileName))
 			{
                 CreateConfigFile();
@@ -227,106 +258,88 @@ namespace ArduinoKeyboard
             //Save the file
 			parser.WriteFile(CNTL_PATH + g_strIniFileName, data);
 			return true;*/
-        }
-        private static void CreateConfigFile()
-        {
-            File.Create(CNTL_PATH + g_strIniFileName).Close();
-            FileIniDataParser parser = new FileIniDataParser();
-            IniData data = new IniData();
+		}
+		private static void CreateConfigFile()
+		{
+			LogInfo("Criando Arquivo de configuração novo");
+			File.Create(CNTL_PATH + g_strIniFileName).Close();
+			FileIniDataParser parser = new FileIniDataParser();
+			IniData data = new IniData();
 
-            data.Sections.AddSection("SERVICO");
-            data.Sections.AddSection("BOTOES");
-            data["SERVICO"].AddKey("SLEEP_NOT_EXIST", "30");
-            data["SERVICO"].AddKey("SLEEP", "1");
-            data["SERVICO"].AddKey("NIVEL_LOG", "SEVERE");
-            data["SERVICO"].AddKey("LOG_DATA", "FALSE");
-            data["BOTOES"].AddKey("BOTAO_REPETE", "true, true, true, true, true, true, true, true, true, true, true");
-            data["BOTOES"].AddKey("TEMPOS_REPETICAO", "10,25,2");
-            parser.WriteFile(CNTL_PATH + g_strIniFileName, data);
-        }
-        public static void RemoveCom(ArduinoConnect connect)
-        {
-            mapConnects.Remove(connect.ComPort);
-        }
-        /// <summary>
-        /// Método que monitora as portas e inicializa as conexões
-        /// </summary>
-        public void Run()
-        {
-            try
-            {
-                joystick = new vJoy();
-                iReport = new vJoy.JoystickState();
-                if (!joystick.vJoyEnabled())
-                {
-                    Log("Main", "joystick não está funcionando");
-                    this.Stop();
-                    return;
-                }
+			data.Sections.AddSection("SERVICO");
+			data.Sections.AddSection("BOTOES");
+			data["SERVICO"].AddKey("SLEEP_NOT_EXIST", "30");
+			data["SERVICO"].AddKey("SLEEP", "1");
+			data["SERVICO"].AddKey("NIVEL_LOG", "SEVERE");
+			data["SERVICO"].AddKey("LOG_DATA", "FALSE");
+			data["BOTOES"].AddKey("BOTAO_REPETE", "true, true, true, true, true, true, true, true, true, true, true");
+			data["BOTOES"].AddKey("TEMPOS_REPETICAO", "10,25,2");
+			parser.WriteFile(CNTL_PATH + g_strIniFileName, data);
+			LogInfo("Arquivo de configuração criado com sucesso");
+		}
+		public static void RemoveCom(ArduinoConnect connect)
+		{
+			LogSevere(String.Format("Connecção {0} sendo removida", connect.ComPort));
+			mapConnects.Remove(connect.ComPort);
+		}
+		/// <summary>
+		/// Método que monitora as portas e inicializa as conexões
+		/// </summary>
+		public void Run()
+		{
+			try
+			{
+				List<String> keys;
+				while (!this.stop)
+				{
+					ArduinoConnect arduinoConnect;
+					bool hasConnected = false;
 
-                VjdStat status = joystick.GetVJDStatus(1);
-                // Acquire the target
-                if ((status == VjdStat.VJD_STAT_OWN) || ((status == VjdStat.VJD_STAT_FREE) && (!joystick.AcquireVJD(1))))
-                {
-                    Log("Main", "Falha ao carregar o joystick 1");
-                    this.Stop();
-                    return;
-                }
-                else
-                {
-                    Log("Main", "Carregou o joystick 1");
-                }
-                joystick.ResetVJD(1);
-                List<String> keys;
-                while (!this.stop)
-                {
-                    ArduinoConnect arduinoConnect;
-                    bool hasConnected = false;
+					List<String> portNames = SerialPort.GetPortNames().ToList<String>();
+					keys = mapConnects.Keys.ToList();
+					//usa assim para poder remover
+					foreach (String key in keys)
+					{
+						if (!portNames.Contains(key))
+						{
+							LogSevere("Porta " + key + " Desconectada");
 
-                    List<String> portNames = SerialPort.GetPortNames().ToList<String>();
-                    keys = mapConnects.Keys.ToList();
-                    //usa assim para poder remover
-                    foreach (String key in keys)
-                    {
-                        if (!portNames.Contains(key))
-                        {
-                            Log("Main", "Porta " + key + " Desconectada");
+							mapConnects[key].Stop();
+							mapConnects.Remove(key);
+						}
+						else if (mapConnects[key].IsConnected())
+						{
+							hasConnected = true;
+						}
+					}
+					//só verifica se não houver conexão já ativa
+					if (!hasConnected)
+					{
+						foreach (string portName in portNames)
+						{
+							if (!mapConnects.ContainsKey(portName))
+							{
 
-                            mapConnects[key].Stop();
-                            mapConnects.Remove(key);
-                        }
-                        else if (mapConnects[key].IsConnected())
-                        {
-                            hasConnected = true;
-                        }
-                    }
-                    //só verifica se não houver conexão já ativa
-                    if (!hasConnected)
-                    {
-                        foreach (string portName in portNames)
-                        {
-                            if (!mapConnects.ContainsKey(portName))
-                            {
+								LogSevere("Criando conexão para a porta " + portName);
+								arduinoConnect = new ArduinoConnect(config, portName);
+								mapConnects.Add(portName, arduinoConnect);
 
-                                Log("Main", "Vai criar conexão para a porta " + portName);
-                                arduinoConnect = new ArduinoConnect(config, portName);
-                                mapConnects.Add(portName, arduinoConnect);
-
-                                Thread thread = new System.Threading.Thread(new ThreadStart(arduinoConnect.ReadFromPort));
-                                thread.Start();
-                            }
-                        }
-                    }
-                    if (ArduinoKeyboardService.G_ShutdownEvent.WaitOne(config.SleepTime))
-                    {
-                        this.stop = true;
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Log("Main", ex.Message);
-            }
-        }
-    }
+								Thread thread = new System.Threading.Thread(new ThreadStart(arduinoConnect.ReadFromPort));
+								thread.Start();
+							}
+						}
+					}
+					if (ArduinoKeyboardService.G_ShutdownEvent.WaitOne(config.SleepTime))
+					{
+						LogSevere("Recebeu comando de parada");
+						this.stop = true;
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				LogSevere(String.Format("Exception capturada no método RUN: Mensagem: [{0}]\nStack:{1}",ex.Message, ex.StackTrace));
+			}
+		}
+	}
 }
